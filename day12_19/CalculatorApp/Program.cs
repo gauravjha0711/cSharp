ad32594ba3971d90b4110b37f724d6163fdb0986
gauravjha0711 <gauravjha0711@gmail.com>
2025-12-19 12:00:50 +0530

    loop done

--- day12_19/CalculatorApp/Program.cs
+++ day12_19/CalculatorApp/Program.cs
@@ -4,35 +4,87 @@ class Program
     int number1, number2, result;
     public static void Main()
     {
-        Program calc = new Program();
-        Console.WriteLine("Select an operation to perform : ");
-        Console.WriteLine("1. Addition");
-        Console.WriteLine("2. Subtraction");
-        Console.WriteLine("3. Multiplication");
-        Console.WriteLine("4. Division");
-        Console.WriteLine("5. Modulus");
-        int choice = Convert.ToInt32(Console.ReadLine());
-        switch(choice)
+        // Program calc = new Program();
+        // Console.WriteLine("Select an operation to perform : ");
+        // Console.WriteLine("1. Addition");
+        // Console.WriteLine("2. Subtraction");
+        // Console.WriteLine("3. Multiplication");
+        // Console.WriteLine("4. Division");
+        // Console.WriteLine("5. Modulus");
+        // int choice = Convert.ToInt32(Console.ReadLine());
+        // switch(choice)
+        // {
+        //     case 1:
+        //         calc.Add();
+        //         break;
+        //     case 2:
+        //         calc.Subtract();
+        //         break;
+        //     case 3:
+        //         calc.Multiply();
+        //         break;
+        //     case 4:
+        //         calc.Divide();
+        //         break;
+        //     case 5:
+        //         calc.Modulus();
+        //         break;
+        //     default:
+        //         Console.WriteLine("Invalid Choice");
+        //         break;
+        // }
+
+
+
+        // int i=0;
+        // while (i <= 10)
+        // {
+        //     Console. WriteLine($"Value of i: {i}");
+        //     i++;
+        // }
+
+
+
+        // int i=10;
+        // while(i > 0)
+        // {
+        //     Console. WriteLine($"Value of i: {i}");
+        //     i--;
+        // }
+
+
+
+        // for(int i=0; i<=10; i++)
+        // {
+        //     Console. WriteLine($"Value of i: {i}");
+        // }
+
+
+        // for(int i=10; i>0; i--)
+        // {
+        //     Console. WriteLine($"Value of i: {i}");
+        // }
+
+
+        int[] arr = {10,20,30,40,50,60,70,80,90,100};
+        // foreach(int i in arr)
+        // {
+        //     Console. WriteLine($"Value of i: {i}");
+        // }
+
+        // for(int i=0; i<arr.Length; i++)
+        // {
+        //     Console. WriteLine($"Value of arr[{i}]: {arr[i]}");
+        // }
+
+        int i=0;
+        while(i < arr.Length)
         {
-            case 1:
-                calc.Add();
-                break;
-            case 2:
-                calc.Subtract();
-                break;
-            case 3:
-                calc.Multiply();
-                break;
-            case 4:
-                calc.Divide();
-                break;
-            case 5:
-                calc.Modulus();
-                break;
-            default:
-                Console.WriteLine("Invalid Choice");
-                break;
+            Console. WriteLine($"Value of arr[{i}]: {arr[i]}");
+            i++;
         }
+
+
     }
     public void Add()
     {
--- day12_19/CalculatorApp/obj/Debug/net10.0/CalculatorApp.AssemblyInfo.cs
+++ day12_19/CalculatorApp/obj/Debug/net10.0/CalculatorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f9e7c03a71a160f8d3eb76f763ee3440362cb4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4226bf6214c2f3948c3e963506884196099c12ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
