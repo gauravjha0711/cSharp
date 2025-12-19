cb0e0cfea4c5ffd4152de823614b016fb3f20af7
gauravjha0711 <gauravjha0711@gmail.com>
2025-12-19 12:37:49 +0530

    Practice set 2 done

--- day12_19/Practice/Program.cs
+++ day12_19/Practice/Program.cs
@@ -77,17 +77,27 @@ class Practice
         // }
 
         // int[] arr = {10, 30, 20};
-        // if(arr[0]>arr[1] && arr[0]>arr[2])
+        // if (arr[0] > arr[1])
         // {
-        //     Console.WriteLine("First element is largest: " + arr[0]);
-        // }
-        // else if(arr[1]>arr[0] && arr[1]>arr[2])
-        // {
-        //     Console.WriteLine("Second element is largest: " + arr[1]);
+        //     if(arr[0] > arr[2])
+        //     {
+        //         Console.WriteLine("Largest element is: " + arr[0]);
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("Largest element is: " + arr[2]);
+        //     }
         // }
         // else
         // {
-        //     Console.WriteLine("Third element is largest: " + arr[2]);
+        //     if(arr[1] > arr[2])
+        //     {
+        //         Console.WriteLine("Largest element is: " + arr[1]);
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("Largest element is: " + arr[2]);
+        //     }
         // }
 
         // int largest = arr[0];
@@ -99,5 +109,140 @@ class Practice
         //     }
         // }
         // Console.WriteLine("Largest element is: " + largest);
+
+
+
+        // 17)Write C#  program to print numbers from 1 to n(user input) using while loop*/
+        // 18)Write C#  program to print even numbers from 1 to n(user input) 
+        // 19)Write C#  program to find the sum  of numbers from 1 to n (user input) using while loop*/
+        // 20)Write C#  program to print multiplication table of given number */
+        // 21) Write C#  program to calculate the power of a given number */
+        // 22)Write C#  program to generate fibbonacci series(0 1 1 2 3 5 8 13 21 34 55...) upto n(user input)*/
+        // 23)Write C#  program to find whether the given number is Armstrong or not
+        // 24)program to print armstrong numbers from n1 to n2
+        // 25) program to check whether the number is prime or not */
+
+
+        // Console.WriteLine("Enter value of n : ");
+        // int n = Convert.ToInt32(Console.ReadLine());
+        // for(int i=1; i<=n; i++)
+        // {
+        //     Console.WriteLine("Value: " + i);
+        // }
+
+        // Console.WriteLine("Enter value of n : ");
+        // int n = Convert.ToInt32(Console.ReadLine());
+        // for(int i=1; i<=n; i++)
+        // {
+        //     if(i % 2 == 0)
+        //     {
+        //         Console.WriteLine("Even Value: " + i);
+        //     }
+        // }
+
+        // int sum = 0;
+        // int i=1;
+        // Console.WriteLine("Enter value of n : ");
+        // int n = Convert.ToInt32(Console.ReadLine());
+        // while (i <= n)
+        // {
+        //     sum += i;
+        //     i++;
+        // }
+        // Console.WriteLine("Sum of numbers from 1 to n is: " + sum);
+
+        // int mul = 1;
+        // int i=1;
+        // Console.WriteLine("Enter value of n : ");
+        // int n = Convert.ToInt32(Console.ReadLine());
+        // while (i <= n)
+        // {
+        //     mul *= i;
+        //     i++;
+        // }
+        // Console.WriteLine("Product of numbers from 1 to n is: " + mul);
+
+        // Console.WriteLine("Enter base number: ");
+        // int baseNum = Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine("Enter exponent: ");
+        // int exponent = Convert.ToInt32(Console.ReadLine());
+        // int result = 1;
+        // for(int i=1; i<=exponent; i++)
+        // {
+        //     result = result * baseNum;
+        // }
+        // Console.WriteLine("Result: " + result);
+
+        // Console.WriteLine("Enter a number for fibonacci : ");
+        // int number = Convert.ToInt32(Console.ReadLine());
+        // int a = 0, b = 1, c;
+        // Console.WriteLine("Fibonacci Series: ");
+        // for(int i=0; i<number; i++)
+        // {
+        //     Console.WriteLine(a);
+        //     c = a + b;
+        //     a = b;
+        //     b = c;
+        // }
+
+        // Console.WriteLine("Enter a number to check Armstrong: ");
+        // int number = Convert.ToInt32(Console.ReadLine());
+        // int originalNumber = number;
+        // int sum = 0;
+        // while(number != 0)
+        // {
+        //     int digit = number % 10;
+        //     sum += digit * digit * digit;
+        //     number /= 10;
+        // }
+        // if(sum == originalNumber)
+        // {
+        //     Console.WriteLine("The number is an Armstrong number.");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("The number is not an Armstrong number.");
+        // }
+
+        // Console.WriteLine("Enter limit n1 for armstrong numbers: ");
+        // int n1 = Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine("Enter limit n2 for armstrong numbers: ");
+        // int n2 = Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine("Armstrong numbers between {0} and {1} are: ", n1, n2);
+        // for(int num = n1; num <= n2; num++)
+        // {
+        //     int sum = 0;
+        //     int temp = num;
+        //     while(temp != 0)
+        //     {
+        //         int digit = temp % 10;
+        //         sum += digit * digit * digit;
+        //         temp /= 10;
+        //     }
+        //     if(sum == num)
+        //     {
+        //         Console.WriteLine(num);
+        //     }
+        // }
+
+        Console.WriteLine("Enter a number to check Prime: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        bool isPrime = true;
+        for(int i=2; i<=number/2; i++)
+        {
+            if(number % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime && number > 1)
+        {
+            Console.WriteLine("The number is Prime.");
+        }
+        else
+        {
+            Console.WriteLine("The number is not Prime.");
+        }
     }
 }
