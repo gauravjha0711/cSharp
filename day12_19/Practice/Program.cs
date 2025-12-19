89e32d9f7b1636ed836a9048e098f4b59bb601c1
gauravjha0711 <gauravjha0711@gmail.com>
2025-12-19 12:17:45 +0530

    Practice set 1 done

---
+++ day12_19/Practice/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+class Practice
+{
+    static void Main()
+    {   
+        // 1)Sample C# program to print a welcome message
+        // 2)Write C# program to read a number user and display it */
+        // 3)Write C# program to read a floating point number from user 
+        // 4)Write C# program to read a string from user and display 
+        // it on the screen 
+        // 5)Write C# program to perform all arithmetic operations*/
+        // 6)Write C# program to find the area of circle */
+        // 7) program to find whether the given number is Even or Odd */
+        // 8)program to find the greatest of 2 numbers */
+        // 9) program to find whether a given number is positive ,negative or zero*/
+        // 10 program to find the greatest of three numbers using nested if*/ 
+        // 11) program to find the greatest of 3 numbers using conditional operator */
+
+
+        Console.WriteLine("Hello, from Gaurav Jha");
+        // Console.WriteLine("Enter integer number: ");
+        // int num = Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine("You entered: " + num);
+        // Console.WriteLine("Enter floating point number: ");
+        // float fnum = Convert.ToSingle(Console.ReadLine());
+        // Console.WriteLine("You entered: " + fnum);
+        // Console.WriteLine("Enter string: ");
+        // string temp = Convert.ToString(Console.ReadLine());
+        // Console.WriteLine("You entered string: " + temp);
+
+        // Console.WriteLine("Enter radius: ");
+        // int radius = Convert.ToInt32(Console.ReadLine());
+        // double area = 3.14 * radius * radius;
+        // Console.WriteLine("Area of circle is: " + area);
+
+        // Console.WriteLine("Enter number: ");
+        // int number = Convert.ToInt32(Console.ReadLine());
+        // if(number % 2 == 0)
+        // {
+        //     Console.WriteLine("The number is even.");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("The number is odd.");
+        // }
+
+        // Console.WriteLine("Enter first number: ");
+        // int num1 = Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine("Enter second number: ");
+        // int num2 = Convert.ToInt32(Console.ReadLine());
+        // if(num1 > num2)
+        // {
+        //     Console.WriteLine("First number is greater.");
+        // }
+        // else if(num2 > num1)
+        // {
+        //     Console.WriteLine("Second number is greater.");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Both numbers are equal.");
+        // }
+
+        // Console.WriteLine("Enter Number: ");
+        // int number = Convert.ToInt32(Console.ReadLine());
+        // if(number < 0)
+        // {
+        //     Console.WriteLine("Negative Number");
+        // }
+        // else if(number == 0)
+        // {
+        //     Console.WriteLine("Zero");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Positive Number");
+        // }
+
+        // int[] arr = {10, 30, 20};
+        // if(arr[0]>arr[1] && arr[0]>arr[2])
+        // {
+        //     Console.WriteLine("First element is largest: " + arr[0]);
+        // }
+        // else if(arr[1]>arr[0] && arr[1]>arr[2])
+        // {
+        //     Console.WriteLine("Second element is largest: " + arr[1]);
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Third element is largest: " + arr[2]);
+        // }
+
+        // int largest = arr[0];
+        // for(int i=1; i<arr.Length; i++)
+        // {
+        //     if(arr[i] > largest)
+        //     {
+        //         largest = arr[i];
+        //     }
+        // }
+        // Console.WriteLine("Largest element is: " + largest);
+    }
+}
