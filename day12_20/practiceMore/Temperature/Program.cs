22f6988901089e2feffd30766ce0e695807590e6
gauravjha0711 <gauravjha0711@gmail.com>
2025-12-20 14:43:22 +0530

    Temperature celsius to fahrenheit

---
+++ day12_20/practiceMore/Temperature/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Program
+{
+    //Temperature Conversion Program from Celsius to Fahrenheit
+    static void Main()
+    {
+        Console.WriteLine("Enter Temperature in Celsius: ");
+        double celsius = Convert.ToDouble(Console.ReadLine());
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        Console.WriteLine($"Temperature in Celsius: {celsius}°C");
+        Console.WriteLine($"Temperature in Fahrenheit: {fahrenheit}°F");
+    }
+}
