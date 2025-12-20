95fffa8b051f525046346dbec167368c4ad2834b
gauravjha0711 <gauravjha0711@gmail.com>
2025-12-20 15:12:31 +0530

    Natutal Number Sum

---
+++ day12_20/practiceMore/NaturalSum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        //Natural number Sum Program
+        Console.WriteLine("Enter a positive integer: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if(num < 1)
+        {
+            Console.WriteLine("Please enter a positive integer greater than zero.");
+        }
+        else
+        {
+            int sum = 0;
+            for(int i = 1; i <= num; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"The sum of natural numbers up to {num} is: {sum}");
+        }
+    }
+}
