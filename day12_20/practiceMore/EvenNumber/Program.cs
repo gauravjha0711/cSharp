ff56302e46048a837f366bb980110ce5f97cf65a
gauravjha0711 <gauravjha0711@gmail.com>
2025-12-20 15:22:03 +0530

    Even Number

---
+++ day12_20/practiceMore/EvenNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter Number of Even Numbers to Display: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"First {n} Even Numbers are: ");
+        for(int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(i * 2);
+        }
+    }
+}
