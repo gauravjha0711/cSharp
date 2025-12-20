82aa65ff40df1b1e24debe9bcf8e5317daa26e51
gauravjha0711 <gauravjha0711@gmail.com>
2025-12-20 15:17:05 +0530

    Grade of Marks

---
+++ day12_20/practiceMore/Grade/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enetr Score: ");
+        int score = Convert.ToInt32(Console.ReadLine());
+        char grade;
+        if(score >= 90)
+        {
+            grade = 'A';
+        }
+        else if(score >= 80)
+        {
+            grade = 'B';
+        }
+        else if(score >= 70)
+        {
+            grade = 'C';
+        }
+        else if(score >= 60)
+        {
+            grade = 'D';
+        }
+        else
+        {
+            grade = 'F';
+        }
+        Console.WriteLine($"Grade: {grade}");
+    }
+}
