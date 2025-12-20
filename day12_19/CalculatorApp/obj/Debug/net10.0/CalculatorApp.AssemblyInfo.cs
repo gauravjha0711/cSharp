4fb5acad7a1c577716ed0ee196905a5fb396463d
gauravjha0711 <gauravjha0711@gmail.com>
2025-12-20 14:38:25 +0530

    Leap Year

--- day12_19/CalculatorApp/obj/Debug/net10.0/CalculatorApp.AssemblyInfo.cs
+++ day12_19/CalculatorApp/obj/Debug/net10.0/CalculatorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1a40c3dcd6de006ff0cea8befeca3cb10f9609e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e1ad28423d22bf247600a6ea8f8a16d983e5bd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ day12_20/practiceMore/LeapYear/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    //Leap Year Program
+    static void Main()
+    {
+        Console.WriteLine("Enter a Year: ");
+        int year = Convert.ToInt32(Console.ReadLine());
+        if((year%4==0 && year%100!=0) || (year % 400 == 0))
+        {
+            Console.WriteLine($"{year} is a Leap Year");
+        }
+        else
+        {
+            Console.WriteLine($"{year} is not a Leap Year");
+        }
+    }
+}
