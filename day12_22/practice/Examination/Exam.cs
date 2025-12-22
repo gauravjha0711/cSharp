616c4fbbe967021c381dcba472a12326c96e2ccb
gauravjha0711 <gauravjha0711@gmail.com>
2025-12-22 15:07:32 +0530

    Examination

---
+++ day12_22/practice/Examination/Exam.cs
@@ -0,0 +1,10 @@
+using System;
+public class Exam
+{
+    public void Addition(int n1, int n2, out int res, out int n3, out int n4)
+    {
+        res = n1 + n2;
+        n3 = n1;
+        n4 = n2;
+    }
+}
---
+++ day12_22/practice/Examination/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class Examination
+{
+    public static void Main()
+    {
+        int n1 = 85;
+        int n2 = 90;
+        int result = 0;
+        int n3=0;
+        int n4=0;
+        Exam exam = new Exam();  //calling default constructor
+        exam.Addition(n1, n2, out result, out n3, out n4);
+        Console.WriteLine($"First number: {n3}, Second number: {n4}");
+        Console.WriteLine($"Addition of {n3} and {n4} is: {result}");
+    }
+}
