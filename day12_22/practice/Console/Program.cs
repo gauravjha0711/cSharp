33e87e49bc77e4c354b5a6a27a5c17844d78a599
gauravjha0711 <gauravjha0711@gmail.com>
2025-12-22 15:17:23 +0530

    Console

---
+++ day12_22/practice/Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class Calc
+{
+    public static void Main()
+    {
+        int n1 = 85;
+        int n2 = 90;
+        int result = 0;
+        int n3=0;
+        int n4=0;
+        Calculator calculator = new Calculator();  //calling default constructor
+        calculator.Addition(n1, n2, out result, out n3, out n4);
+        Console.WriteLine($"First number: {n3}, Second number: {n4}");
+        Console.WriteLine($"Addition of {n3} and {n4} is: {result}");
+    }
+}
