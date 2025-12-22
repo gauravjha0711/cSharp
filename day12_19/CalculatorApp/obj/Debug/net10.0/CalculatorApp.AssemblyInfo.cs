6feed99585bb1c15750eedc1e1d33503369d8ad8
gauravjha0711 <gauravjha0711@gmail.com>
2025-12-22 11:22:14 +0530

    day12_22 Prime number

--- day12_19/CalculatorApp/obj/Debug/net10.0/CalculatorApp.AssemblyInfo.cs
+++ day12_19/CalculatorApp/obj/Debug/net10.0/CalculatorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e1ad28423d22bf247600a6ea8f8a16d983e5bd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff56302e46048a837f366bb980110ce5f97cf65a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ day12_22/primeNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter Number for prime number to display: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        if(n==0) Console.WriteLine("No prime numbers available");
+        else
+        {
+            Console.WriteLine($"Prime Numbers upto {n} are: ");
+            bool[] prime = new bool[n+1];
+            Array.Fill(prime, true);
+            prime[0] = prime[1] = false;
+            for(int i = 2; i <= n; i++)
+            {
+                if (prime[i])
+                {
+                    Console.WriteLine(i);
+                    for(int j = i * i; j <= n; j += i)
+                    {
+                        prime[j] = false;
+                    }
+                }
+            }
+        }
+    }
+}
