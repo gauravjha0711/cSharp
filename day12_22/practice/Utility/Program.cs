ae4426bbe3698fd95a37d1b840b0e10fca87f43e
gauravjha0711 <gauravjha0711@gmail.com>
2025-12-22 15:14:05 +0530

    Utility

---
+++ day12_22/practice/Utility/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Runtime.CompilerServices;
+public class Program
+{
+    public static void Main()
+    {
+        Utility utility = new Utility();  //calling default constructor
+        utility.num1 = 15;
+        utility.num2 = 25;
+        utility.Result = utility.num1 + utility.num2;
+        Console.WriteLine($"Result of adding {utility.num1} and {utility.num2} is: {utility.Result}");
+        Console.WriteLine($"Number1: {utility.num1}");
+        Console.WriteLine($"Number2: {utility.num2}");
+    }
+}
---
+++ day12_22/practice/Utility/Utility.cs
@@ -0,0 +1,8 @@
+using System;
+using System.IO.Pipelines;
+public class Utility
+{
+    public int num1 { get; set; }
+    public int num2 { get; set; }
+    public int Result { get; set; }
+}
