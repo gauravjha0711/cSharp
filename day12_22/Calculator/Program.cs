ad01292c045f79c6e367414409e4339bef6cf220
gauravjha0711 <gauravjha0711@gmail.com>
2025-12-22 12:10:35 +0530

    Calculator with oops

--- day12_22/Calculator/Program.cs
+++ day12_22/Calculator/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Calculator calculator = new Calculator();  //calling default constructor
+        int number1 = 20;
+        int number2 = 30;
+        int sum = calculator.Add(number1, number2);
+        Console.WriteLine($"Sum of {number1} and {number2} is: {sum}");
+
+        calculator.Subtract(number2, number1);
+
+        int multiply = calculator.Multiply();
+        Console.WriteLine($"Multiplication of {number1} and {number2} is: {multiply}");
+
+        Calculator calculator1 = new Calculator(10, 2);  //calling parameterized constructor
+        calculator1.Divide();
+    }
+}
