ba49cee9260a224b3d4311c21e62006c31a52740
gauravjha0711 <gauravjha0711@gmail.com>
2025-12-29 12:10:34 +0530

    Reverse array Practice

---
+++ day12_29/practice/Array/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Console.Write("Enter size of array: ");
+        // int size = Convert.ToInt32(Console.ReadLine());
+        // int[] numbers = new int[size];
+        // Console.WriteLine("Enter array elements:");
+        // for (int i = 0; i < size; i++)
+        // {
+        //     Console.Write($"Element {i}: ");
+        //     numbers[i] = Convert.ToInt32(Console.ReadLine());
+        // }
+        // Console.Write("Elements in the array are: ");
+        // for (int i = 0; i < size; i++)
+        // {
+        //     Console.Write(numbers[i] + " ");
+        // }
+
+
+        //reverse array
+        Console.Write("Enter size of array: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[] numbers = new int[size];
+        Console.WriteLine("Enter array elements:");
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write($"Element {i}: ");
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine("The Original array is:");
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("The Reversed array is:");
+        int left=0;
+        int right=size-1;
+        while(left<right)
+        {
+            int temp = numbers[left];
+            numbers[left] = numbers[right];
+            numbers[right] = temp;
+            left++;
+            right--;
+        }
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    }
+}
