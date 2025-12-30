0f388103d4b9ee1ad326148d3e12c825456767cd
gauravjha0711 <gauravjha0711@gmail.com>
2025-12-30 12:56:22 +0530

    String array practice:

---
+++ day12_30/Practice/StringPrac/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Console.WriteLine("Enter a string: ");
+        // string temp = Console.ReadLine();
+        // Console.WriteLine($"You enter: {temp}");
+
+        string[] StringArray = new string[5];
+        Console.WriteLine("Enter 5 string: ");
+        for(int i = 0; i < 5; i++)
+        {   
+            Console.Write($"Enter String at index {i}: ");
+            StringArray[i] = Console.ReadLine();
+        }
+        Console.WriteLine("All String are: ");
+        for(int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(StringArray[i]);
+        }
+    }
+}
