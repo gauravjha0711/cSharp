2bcf156a6d59abbccfd784ebf11db222d21fb001
gauravjha0711 <gauravjha0711@gmail.com>
2025-12-30 12:22:36 +0530

    Jagged Array

---
+++ day12_30/JaggedArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        int[][] jaggedArray = new int[4][];
+        jaggedArray[0] = new int[] { 1, 2, 3 };
+        jaggedArray[1] = new int[] { 4, 5 };
+        jaggedArray[2] = new int[] { 6, 7, 8, 9 };
+        jaggedArray[3] = new int[] { 10, 11, 12, 13, 14 };
+
+        //display jagged array
+        for(int i = 0; i < jaggedArray.Length; i++)
+        {
+            Console.Write("Row " + i + ": ");
+            for(int j = 0; j < jaggedArray[i].Length; j++)
+            {
+                Console.Write(jaggedArray[i][j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
