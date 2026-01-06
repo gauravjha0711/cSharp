0f9ad48898c3f07fe0860da7c9a1e03080b7ddc0
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-06 12:33:26 +0530

    Nested Try

--- day1_6/Exception/Program.cs
+++ day1_6/Exception/Program.cs
@@ -9,19 +9,49 @@ public class Program
         // result = num1 / num2;
         // Console.WriteLine($"The result of division of {num1} by {num2} is: {result}");
 
+        // try
+        // {
+        //     Console.WriteLine("Enter first number:");
+        //     num1 = Convert.ToInt32(Console.ReadLine());
+        //     Console.WriteLine("Enter second number:");
+        //     num2 = Convert.ToInt32(Console.ReadLine());
+        //     result = num1 / num2;
+        //     Console.WriteLine($"The result of division of {num1} by {num2} is: {result}");
+        // }
+        // catch (DivideByZeroException ex)
+        // {
+        //     Console.WriteLine("Error: Division by zero is not allowed.");
+        //     Console.WriteLine(ex.Message);
+        // }
+        // catch (FormatException ex)
+        // {
+        //     Console.WriteLine("Error: Invalid input format. Please enter numeric values.");
+        //     Console.WriteLine(ex.Message);
+        // }
+        // finally
+        // {
+        //     Console.WriteLine("Execution completed.");
+        // }
+
+
+
+        //nested try-catch
         try
         {
             Console.WriteLine("Enter first number:");
             num1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter second number:");
             num2 = Convert.ToInt32(Console.ReadLine());
-            result = num1 / num2;
-            Console.WriteLine($"The result of division of {num1} by {num2} is: {result}");
-        }
-        catch (DivideByZeroException ex)
-        {
-            Console.WriteLine("Error: Division by zero is not allowed.");
-            Console.WriteLine(ex.Message);
+            try
+            {
+                result = num1 / num2;
+                Console.WriteLine($"The result of division of {num1} by {num2} is: {result}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Error: Division by zero is not allowed.");
+                Console.WriteLine(ex.Message);
+            }
         }
         catch (FormatException ex)
         {
