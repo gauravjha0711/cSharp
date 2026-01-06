8e9a2eb3aa98380df0614202010020d34dc9c468
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-06 15:10:06 +0530

    Practice try catch Temparature

---
+++ day1_6/Practice/TemperatureConversion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Enter temperature: ");
+            double temp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Convert to (C/F): ");
+            char scale = Char.ToUpper(Convert.ToChar(Console.ReadLine()));
+            if(scale == 'C')
+            {
+                double fahrenheit = temp*9/5 + 32;
+                Console.WriteLine($"Temperature in fahrenheit: {fahrenheit:F2} F");
+            }
+            else if(scale == 'F')
+            {
+                double celc = (temp - 32) * 5 / 9;
+                Console.WriteLine($"Temperature in celsius: {celc:F2} C");
+            }
+            else
+            {
+                Console.WriteLine("Invalid conversion type. Please enter 'C' or 'F'.");
+            }
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine("Error: Invalid input provided.");
+            Console.WriteLine("Exception Message: " + e.Message);
+        }
+    }
+}
