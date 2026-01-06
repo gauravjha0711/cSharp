4bfd73cb4b0ad93b783d2f26328b9aa23c06fb61
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-06 10:22:30 +0530

    Indexer

---
+++ day1_6/Indexer/Employee.cs
@@ -0,0 +1,30 @@
+using System;
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    string[] str = new string[5];
+    public string this[int index]
+    {
+        get { return str[index]; }
+        set { str[index] = value; }
+    }
+    public int this[string sr]
+    {
+        get
+        {
+            int index = 0;
+            while (index < 5)
+            {
+                if (str[index] == sr)
+                    return index;
+                index++;
+            }
+            return index;
+        }
+    }
+    public override string ToString()
+    {
+        return $"Id: {Id}, Name: {Name}";
+    }
+}
---
+++ day1_6/Indexer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Indexer
+{
+    public static void Main()
+    {
+        Employee employee = new Employee();
+        employee[0] = "String 1";
+        employee[1] = "String 2";
+        employee[2] = "String 3";
+        employee[3] = "String 4";
+        employee[4] = "String 5";
+
+        // employee[5] = "String 6"; // This will throw an exception IndexOutOfRange
+        Console.WriteLine("Accessing values using indexer:");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(employee[i]);
+        }
+
+        Console.WriteLine($"Value at third index: {employee[2]}");
+        Console.WriteLine($"The value 'String 4' is at index: {employee["String 4"]}");
+    }
+}
