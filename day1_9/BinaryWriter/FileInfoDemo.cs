a4308320e7b4a6d4ab9bef5f58ebd8a50b54f940
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-09 11:38:08 +0530

    FileInfo

---
+++ day1_9/BinaryWriter/FileInfoDemo.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+public class FileInfoDemo
+{
+    public static void ShowFileInfo()
+    {
+        //The FileInfo class is used to create, copy, delete, move, and open files, and helps to create FileStream objects.
+        string path  = @"MyTestFile.txt";
+        FileInfo fileInfo = new FileInfo(path);
+        File.Create(path);
+        {
+            Console.WriteLine("File has been created.");
+        }
+
+        //The CreateText method is used to create a text file and returns a StreamWriter that can be used to write text to the file.
+        FileInfo fi = new File(@"MyTestFileCreatedText.txt");
+        StreamWriter str = fi.CreateText();
+        str.WriteLine("This is my first line.");
+        Console.WriteLine("Text written to file successfully.");
+        str.Close();
+
+        //This method is used to delete a file
+        FileInfo fi = new FileInfo(@"MyInfoFile2.txt");
+        fi.Delete();
+        Console.WriteLine("File Deleted successfully");
+
+
+        //The CopyTo method is used to copy a existing file to a new file.
+        string path = @"MyTestFile7.txt";
+        string path2 = @"NewFile.txt";
+        FileInfo fi1 = new FileInfo(path);
+        FileInfo fi2 = fi1.CopyTo(path2);
+        fi1.CopyTo(path2);
+        Console.WriteLine($"{path} File copied {path2} successfully.");
+    }
+}   
