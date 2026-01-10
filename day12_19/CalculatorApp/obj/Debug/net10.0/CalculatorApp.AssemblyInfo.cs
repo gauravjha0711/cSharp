3d489a6f09e21edfbfd7532cf56bdce01d743ca1
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-10 14:34:08 +0530

    Student Name Collection

--- day12_19/CalculatorApp/obj/Debug/net10.0/CalculatorApp.AssemblyInfo.cs
+++ day12_19/CalculatorApp/obj/Debug/net10.0/CalculatorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2accc004f7cc1c46a9aa4bf97fcb8d4b1e88d3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dba97961994ac2a3972e244fdf8eca489623e07f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ day1_10/Practice/StudentCollection/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+public class Program
+{
+    public static void Main()
+    {
+        ArrayList students = new ArrayList();
+        StringBuilder ans = new StringBuilder();
+        while (true)
+        {
+            Console.Write("Enter Student Name: ");
+            string name = Console.ReadLine();
+            if (name.ToLower().Equals("stop"))
+            {
+                break;
+            }
+            if (!isValidName(name))
+            {
+                continue;
+            }
+            if (isDuplicate(students,name))
+            {
+                ans.AppendLine($"{name} is already in the collection.");
+            }
+            else
+            {
+                students.Add(name);
+                ans.AppendLine($"{name} added to the collection.");
+            }
+        }
+        Console.WriteLine(ans.ToString());
+        Console.WriteLine("Unique Students name entered: ");
+        DisplayStudentNames(students);
+    }
+    private static bool isValidName(string name)
+    {
+        return !string.IsNullOrWhiteSpace(name);
+    }
+    private static bool isDuplicate(ArrayList students, string name)
+    {
+        foreach (object item in students)
+        {
+            if (item.ToString().Equals(name, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    private static void DisplayStudentNames(ArrayList students)
+    {
+        foreach (var student in students)
+        {
+            Console.WriteLine(student);
+        }
+    }
+}
