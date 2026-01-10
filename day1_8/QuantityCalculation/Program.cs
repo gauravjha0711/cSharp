dba97961994ac2a3972e244fdf8eca489623e07f
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-10 12:14:17 +0530

    day1_8 Quantity Calculation

---
+++ day1_8/QuantityCalculation/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+public class InvalidPriceException : Exception
+{
+    public InvalidPriceException(string message) : base(message)
+    {
+    }
+}
+public class InvalidQuantityException : Exception
+{
+    public InvalidQuantityException(string message) : base(message)
+    {
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            Console.Write("Enter Price: ");
+            decimal price = decimal.Parse(Console.ReadLine());
+            Console.Write("Enter Quantity: ");
+            int quantity = int.Parse(Console.ReadLine());
+            if(price <= 0)
+            {
+                throw new InvalidPriceException("Price must be greater than zero.");
+            }
+            if(quantity <= 0)
+            {
+                throw new InvalidQuantityException("Quantity must be greater than zero.");
+            }
+            decimal TotalCost = price * quantity;
+            Console.WriteLine($"Total Cost: {TotalCost}");
+        }
+        catch (InvalidPriceException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch( InvalidQuantityException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Please enter valid number.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
