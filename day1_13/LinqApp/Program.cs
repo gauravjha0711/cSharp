b99f5b8487a3b6f71ce233641933c5ac187fae09
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-13 15:19:23 +0530

    Lambda function in linq

---
+++ day1_13/LinqApp/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        // Console.WriteLine("Heello, LINQ!");
+        //DataSource;
+        // string[] names = {"gaurav","kundan","krishna","shubham","john"};
+
+        // //write the query
+        // //IEnumerable<string> names
+        // // var name = from s in names where s.Contains('a') select s;
+        // var name = names.Where(s=>s.Contains('a'));
+        // var name2 = name.Where(s=>s.Length>6);
+        // //execute the query
+        // foreach(string s in name)
+        // {
+        //     Console.WriteLine(s);
+        // }
+        // Console.WriteLine("--------");
+        // foreach(string s in name2)
+        // {
+        //     Console.WriteLine(s);
+        // }
+
+
+
+        IList<string> stringList = new List<string>()
+        {
+            "c# tutorial",
+            "java tutorial",
+            "python tutorial",
+            "learn c++",
+            "java",
+        };
+        var result = stringList.Where(s=>s.Contains("tutorial"));
+        foreach(var str in result)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
