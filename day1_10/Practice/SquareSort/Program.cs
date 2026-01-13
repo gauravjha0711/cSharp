25c1fa78a58d1c185a41f57dcff9ad9ab7e1286f
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-14 00:37:00 +0530

    Practice Square and sort

---
+++ day1_10/Practice/SquareSort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter numbers space separated: ");
+        string input = Console.ReadLine();
+        string[] parts = input.Split(' ');
+        List<int> numbers = new List<int>();
+        foreach(string part in parts)
+        {
+            if(int.TryParse(part,out int num))
+            {
+                numbers.Add(num);
+            }
+            else
+            {
+                Console.WriteLine($"'{part}' is not a valid integer");
+                return;
+            }
+        }
+        List<int> squaredNumbers = new List<int>();
+        foreach(int number in numbers)
+        {
+            squaredNumbers.Add(number * number);
+        }
+        //descending order
+        squaredNumbers.Sort((a,b)=>b.CompareTo(a));
+        Console.WriteLine("Sorted Squared Numbers: "+string.Join(" ",squaredNumbers));
+    }
+}
