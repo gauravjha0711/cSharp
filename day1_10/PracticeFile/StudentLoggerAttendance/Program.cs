baaa4352c1eaa663aae3b13f03520701ed93241b
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-14 10:10:09 +0530

    day1_10 Practice File

---
+++ day1_10/PracticeFile/StudentLoggerAttendance/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+public class Program
+{
+    public static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Log Student Attendance. ");
+            Console.WriteLine("1. Add Attendance. ");
+            Console.WriteLine("2. View Attendance. ");
+            Console.Write("Enter your choice (1 or 2): ");
+            string choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                AddStudentAttendance();
+            }
+            else if(choice == "2")
+            {
+                ViewAttendance();
+            }
+            else if(choice == "3")
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice.");
+            }
+        }
+    }
+    public static void AddStudentAttendance()
+    {
+        Console.Write("Enter Student ID: ");
+        int id = int.Parse(Console.ReadLine());
+        Console.Write("Enter Student Name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter Status (Present/Absent): ");
+        string status = Console.ReadLine();
+        string logEntry = $"{DateTime.Now.ToShortDateString()} | {id} | {name} | {status}";
+        FileStream fs = new FileStream("attendance.txt", FileMode.Append, FileAccess.Write);
+        StreamWriter sw = new StreamWriter(fs);
+        sw.WriteLine(logEntry);
+        sw.Close();
+        fs.Close();
+    }
+    public static void ViewAttendance()
+    {
+        Console.WriteLine("Attendance Records:");
+        if(!File.Exists("attendance.txt"))
+        {
+            Console.WriteLine("No attendance records found.");
+            return;
+        }
+        else
+        {
+            FileStream fs = new FileStream("attendance.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string line;
+            while((line=sr.ReadLine())!=null)
+            {
+                Console.WriteLine(line);
+            }
+            sr.Close();
+            fs.Close();
+        }
+    }
+}
