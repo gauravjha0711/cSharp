e12b81f562a01f1801cfb5e1c3c691f706301769
gauravjha0711 <gauravjha0711@gmail.com>
2026-01-19 10:28:47 +0530

    UserAuthentication

---
+++ day1_14/Practice/UserAuthentication/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+public class PasswordMismatchException : Exception
+{
+    public PasswordMismatchException(string message) : base(message){}
+}   
+public class User
+{
+    public string Name { get; set; }
+    public string Password { get; set; }
+    public string ConfirmationPassword { get; set; }
+}
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter User Name:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Enter Password:");
+        string password = Console.ReadLine();
+        Console.WriteLine("Enter Confirmation Password:");
+        string confirmPassword = Console.ReadLine();
+        User user = ValidatePassword(name, password, confirmPassword);
+        Console.WriteLine("User authenticated successfully.");
+    }
+    public static User ValidatePassword(string name, string password, string confirmPassword)
+    {
+        User user = new User()
+        {
+            Name = name,
+            Password = password,
+            ConfirmationPassword = confirmPassword
+        };
+        if(user.Password == user.ConfirmationPassword)
+        {
+            return user;
+        }
+        else
+        {
+            throw new PasswordMismatchException("Password entered does not match.");
+        }
+    }
+}
